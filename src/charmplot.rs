8837945a8abcb87a7079d9b37dc1cb01e7dc313b
Ben Lambert <bennett-spencer.lambert@pierer-innovation.com>
2023-11-27 08:50:28 +0100

    add Plugin trait todo

--- src/charmplot.rs
+++ src/charmplot.rs
@@ -1,6 +1,8 @@
-use crate::plot_rose::plot_rose;
 use crate::plot_bar::plot_bar;
+use crate::plot_rose::plot_rose;
 use crate::plot_scatter::plot_scatter;
+use nu_plugin::{EvaluatedCall, LabeledError, Plugin};
+use nu_protocol::{Category, PluginSignature, SyntaxShape, Type, Value};
 
 #[derive(Default)]
 pub struct CharmPlot;
@@ -18,29 +20,39 @@ impl CharmPlot {
         name: &str,
         call: &EvaluatedCall,
         input: &Value,
-        labels: Vec<Value>,
-        values: Vec<Value>
-    ) -> Result<Value, LabeledError> {
-        plot_rose(name, call, input, labels, values)
+        // labels: Vec<Value>,
+        // values: Vec<Value>
+    ) {
+        debug_print_vals(name, call, input);
     }
     pub fn scatter_plot(
         &self,
         name: &str,
         call: &EvaluatedCall,
         input: &Value,
-        x: Vec<Value>,
-        y: Vec<Value>
-    )-> Result<Value, LabeledError> {
-        plot_scatter(name, call, input, x, y)
+        // x: Vec<Value>,
+        // y: Vec<Value>
+    ) {
+        debug_print_vals(name, call, input);
     }
     pub fn bar_plot(
         &self,
         name: &str,
         call: &EvaluatedCall,
         input: &Value,
-        labels: Vec<Value>,
-        values: Vec<Value>
-    )-> Result<Value, LabeledError> {
-        plot_bar(name, call, input, labels, values)
+        // labels: Vec<Value>,
+        // values: Vec<Value>
+    ) {
+        debug_print_vals(name, call, input);
     }
 }
+
+impl Plugin for CharmPlot {
+    // TODO
+}
+
+fn debug_print_vals(name: &str, call: &EvaluatedCall, input: &Value) {
+    println!("name: {0}, call: {1}, input: {2}", name, call, input)
+}
+
+// Normally these funcs return Result<Value, LabeledError>
